8d563ba65880a80a347582c43075353455c023d2
Balajir18 <balaji01817@gmail.com>
2025-12-14 19:58:50 +0000

    Edit userdata.sh

--- infrastructure/userdata.sh
+++ infrastructure/userdata.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -eux
 
 dnf install -y nodejs git
 
@@ -7,4 +6,5 @@ cd /home/ec2-user
 git clone https://gitlab.com/cloudinfra1/nodejs-aws-devops.git
 cd nodejs-aws-devops/application
 
-nohup node app.js > /home/ec2-user/node.log 2>&1 &
+npm install
+nohup node app.js &
