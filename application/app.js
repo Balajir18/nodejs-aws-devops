b8db410e2b473c5530624b576aa697e194a27a06
Balajir18 <balaji01817@gmail.com>
2025-12-14 18:52:43 +0000

    Edit app.js

--- application/app.js
+++ application/app.js
@@ -3,45 +3,11 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send(`
-    <html>
-      <head>
-        <title>My DevOps Project</title>
-        <style>
-          body {
-            background: linear-gradient(to right, #667eea, #764ba2);
-            font-family: Arial, sans-serif;
-            color: white;
-            text-align: center;
-            padding-top: 100px;
-          }
-          h1 {
-            font-size: 50px;
-          }
-          p {
-            font-size: 22px;
-          }
-          .box {
-            background: rgba(0, 0, 0, 0.3);
-            padding: 30px;
-            margin: auto;
-            width: 60%;
-            border-radius: 15px;
-          }
-        </style>
-      </head>
-      <body>
-        <div class="box">
-          <h1>🚀 Node.js App Deployed Successfully!</h1>
-          <p>Deployed on AWS EC2</p>
-          <p>Using Terraform & GitLab CI/CD</p>
-          <p>👨‍💻 DevOps Project</p>
-        </div>
-      </body>
-    </html>
+    <h1 style="color:green;">🚀 Node.js App Running Successfully</h1>
+    <p>AWS EC2 | Terraform | GitLab CI/CD</p>
   `);
 });
 
-// 🔴 IMPORTANT CHANGE IS HERE
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
