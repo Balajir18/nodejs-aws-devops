ca24cccdb5325ca15877ec565c30bd2cd79fc3cc
Balajir18 <balaji01817@gmail.com>
2025-12-14 19:25:07 +0000

    Edit userdata.sh

--- infrastructure/userdata.sh
+++ infrastructure/userdata.sh
@@ -1,21 +1,19 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
+set -euxo pipefail
 
-# log everything
+# log user-data execution
 exec > /var/log/userdata.log 2>&1
 
-# install node & git (AL2023 safe)
+# Amazon Linux 2023 uses dnf
+dnf update -y
 dnf install -y nodejs git
 
-# go to home
 cd /home/ec2-user
 
-# clone repo
 git clone https://gitlab.com/cloudinfra1/nodejs-aws-devops.git
 
 cd nodejs-aws-devops/application
 
-# install dependencies
 npm install
 
-# start app in background (bulletproof)
 nohup node app.js &
