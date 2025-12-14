bf800785a28ffa6242771a93d166a3b8563df53c
Balajir18 <balaji01817@gmail.com>
2025-12-14 19:41:30 +0000

    Edit app.js

--- application/app.js
+++ application/app.js
@@ -1,31 +1,8 @@
-const express = require("express");
-const app = express();
+const http = require("http");
 
-app.get("/", (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>My DevOps Project</title>
-        <style>
-          body {
-            background: linear-gradient(to right, #667eea, #764ba2);
-            font-family: Arial, sans-serif;
-            color: white;
-            text-align: center;
-            padding-top: 100px;
-          }
-          h1 {
-            font-size: 50px;
-          }
-          p {
-            font-size: 22px;
-          }
-          .box {
-            background: rgba(0, 0, 0, 0.3);
-            padding: 30px;
-            margin: auto;
-            width: 60%;
-            border-radius: 15px;
-          }
-        </style>
-      </head>
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Node is running on EC2 🚀");
+}).listen(3000, "0.0.0.0");
+
+console.log("Server running on port 3000");
