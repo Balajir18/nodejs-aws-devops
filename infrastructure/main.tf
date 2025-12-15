a169da46248972ef55d8d9815cfe9d10b51d688c
Balajir18 <balaji01817@gmail.com>
2025-12-15 03:48:26 +0000

    Edit main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,14 +2,19 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "node_app" {
-  ami           = var.ami_id
+resource "aws_instance" "app" {
+  ami           = var.ami
   instance_type = "t2.micro"
   key_name      = var.key_name
 
   user_data = file("${path.module}/userdata.sh")
 
   tags = {
-    Name = "NodeApp-Server"
+    Name = "Balaji-DevOps-EC2"
   }
 }
+
+output "public_ip" {
+  value = aws_instance.app.public_ip
+}
+
