a0f6e2d5e888603f0ec20e08a7bade0a96e2db81
Balajir18 <balaji01817@gmail.com>
2025-12-15 05:13:14 +0000

    Edit userdata.sh

--- infrastructure/userdata.sh
+++ infrastructure/userdata.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-
+dnf update -y
 dnf install -y docker
+
 systemctl start docker
 systemctl enable docker
 
-docker run -d -p 3000:3000 --name balaji-app \
-  docker.io/YOUR_DOCKERHUB_USERNAME/balaji-express-app:latest
+sleep 15
+
+docker pull docker.io/balaji01817/balaji-express-app:latest
+
+docker rm -f balaji-app || true
+
+docker run -d \
+  --restart always \
+  -p 3000:3000 \
+  --name balaji-app \
+  docker.io/balaji01817/balaji-express-app:latest
